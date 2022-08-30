7ae99ff1dcfd2f2ac4d768f6af0398cc4deaa4e0
cyrusjmy <112502011+cyrusjmy@users.noreply.github.com>
2022-08-31 00:53:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ export const config = {
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
+      {name: '宝贝', id: "oLGPa5_hv6XJb6_0fo2QqT8R7yjc"}, 
       {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
       {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
       {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
